d4802532e30772b3c82d9ebf374a33a51792acf1
Samuele-Chioda <samuelechioda@gmail.com>
2024-05-30 20:31:05 +0200

    addTodo

--- js/script.js
+++ js/script.js
@@ -3,17 +3,24 @@ createApp({
     data() {
         return {
             todolist: [
-                { text: 'fai allenamento', done: true },
+                { text: 'completa allenamento', done: true },
                 { text: 'fai la spesa', done: false },
-                { text: 'fai esercizio', done: false },
-                { text: 'fai riposo pomeridiano', done: false },
-                { text: 'fai lavoro', done: false }
-            ]
+                { text: 'esegui un esercizio', done: false },
+                { text: 'fai riposo pomeridiano', done: true },
+                { text: 'porta a termine il lavoro', done: false }
+            ],
+            newTodo:''
         }
     },
     methods: {
         removeTodo(index) {
             this.todolist.splice(index, 1);
+        },
+        addTodo() {
+            if (this.newTodo.trim() !== '') {
+                this.todolist.push({text: this.newTodo, done: false});
+                this.newTodo='';
+            }
         }
     }
 }).mount('#app');
